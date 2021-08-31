f5c7288235b0d02f2be6f944177d9ce9b5badca3
iMezemz <iMezemz@users.noreply.github.com>
2021-08-31 15:28:50 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Users Table</h1>
+    <label for="users"><h3>Choose User :</h3></label>
+    <select @change="handleChange" v-model="selectedUser">
+      <option value="v-all">View All</option>
+      <option
+        :key="user.id"
+        v-for="user in this.users"
+        :value="user.first_name"
+      >
+        {{ user.first_name + " " + user.last_name }}
+      </option>
+    </select>
+  </div>
+  <hr />
+  <table border="3px">
+    <tr>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Email</th>
+    </tr>
+    <tr :key="user.id" v-for="user in this.filteredUsers">
+      <th>{{ user.first_name }}</th>
+      <th>{{ user.last_name }}</th>
+      <th>{{ user.email }}</th>
+    </tr>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      users: {},
+      filteredUsers: {},
+      selectedUser: null,
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  updated() {
+    console.log(this.selectedUser);
+  },
+  methods: {
+    async fetchUsers() {
+      const response = await fetch("https://reqres.in/api/users");
+      const json = await response.json();
+      this.users = json.data;
+      this.filteredUsers = this.users;
+    },
+    handleChange() {
+      if (this.selectedUser == "v-all") {
+        this.filteredUsers = this.users;
+      } else if (this.selectedUser != null) {
+        this.filteredUsers = this.users.filter((user) => {
+          return user.first_name == this.selectedUser;
+        });
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
